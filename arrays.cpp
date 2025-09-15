994921d602d5eae6aeae87032cf67f90a7ffdab6
Lateefah camacho <lateefah.camacho07@gmail.com>
2025-09-14 18:51:18 -0700

    create dynamic array

---
+++ arrays.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+// Function to receive a dynamic double array to populate with data
+void enterArrayData(double* arr, int size) {
+    cout << "Enter " << size << "double values:"<<endl;
+    for(int i = 0; i < size; i++) {
+        cout <<"Value " << i+1 <<": ";
+    }
+}
+
+
+
+
+int main()
+{
+    /* code */
+    return 0;
+}
+
+
