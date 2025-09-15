329635cdf9228fe1efdf1541f2c3d849fa34e41b
Lateefah camacho <lateefah.camacho07@gmail.com>
2025-09-14 19:21:07 -0700

    missing cin

--- arrays.cpp
+++ arrays.cpp
@@ -12,8 +12,9 @@ void enterArrayData(double* arr, int size) {
 void outputArrayData (double* arr, int size) {
     cout<<"Array Contents: ";
     for(int i = 0; i < size; i++) {
-        cout <<
+        cout << *(arr + i) <<" ";
     }
+    cout<<endl;
 }
 
 //function to calculate and return the sum of array
@@ -29,7 +30,16 @@ double sumArray(double* arr, int size) {
 int main()
 {
     const int SIZE = 5;
-   
+   double* arr = new double[SIZE];
+
+   //Call functions to populate output, and sum the array
+   enterArrayData(arr, SIZE);
+   outputArrayData(arr, SIZE);
+
+   double total = sumArray(arr, SIZE);
+   cout << "Sum of Array elements: " <<total <<endl;
+
+   delete[] arr;
 
     return 0;
 }
